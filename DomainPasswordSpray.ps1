86dbe92ee65f545dcee860046f966b71a64dc00b
ViperOne <68926315+The-Viper-One@users.noreply.github.com>
2023-03-12 13:53:15 +0000

    Update DomainPasswordSpray.ps1

--- DomainPasswordSpray.ps1
+++ DomainPasswordSpray.ps1
@@ -566,6 +566,7 @@ function Get-ObservationWindow($DomainEntry)
 {
     # Get account lockout observation window to avoid running more than 1
     # password spray per observation window.
+    $ErrorActionPreference = "SilentlyContinue"
     $lockObservationWindow_attr = $DomainEntry.Properties['lockoutObservationWindow']
     $observation_window = $DomainEntry.ConvertLargeIntegerToInt64($lockObservationWindow_attr.Value) / -600000000
     return $observation_window
